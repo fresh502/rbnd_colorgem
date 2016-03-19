d0c9ec3d3ca85d6e8f84c437fc25640ae29c8649
fresh502 <fresh502@gmail.com>
2016-03-19 16:01:39 +0900

    Fix light_gery color code and use my own color methods for String.sample_colors

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,5 +1,5 @@
 class String
-	@@colors_n_code = {red: '31', green: '32', yellow: '33', blue: '34', pink: '95', light_blue: '94', white: '97', light_grey: '92',
+	@@colors_n_code = {red: '31', green: '32', yellow: '33', blue: '34', pink: '95', light_blue: '94', white: '97', light_grey: '37',
 		black: '30'}
 	def self.create_colors
 		@@colors_n_code.each  do |color, code|
@@ -11,8 +11,11 @@ class String
 		@@colors_n_code.keys
 	end
 	def self.sample_colors
-		@@colors_n_code.each do |color, code|
-			puts "This is \e[#{code}m#{color}\e[0m"
+		# @@colors_n_code.each do |color, code|
+		# 	puts "This is \e[#{code}m#{color}\e[0m"
+		# end
+		@@colors_n_code.keys.each do |color|
+			puts "This is #{color.to_s}".send(color)
 		end
 	end
 end
